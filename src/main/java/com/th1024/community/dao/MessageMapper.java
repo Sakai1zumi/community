6d18b14d8c7b7e1a67c33528b037efe9e705f385
Sakai1zumi <izumi0527@126.com>
2022-08-16 02:29:25 +0300

    2022_0816

--- src/main/java/com/th1024/community/dao/MessageMapper.java
+++ src/main/java/com/th1024/community/dao/MessageMapper.java
@@ -35,4 +35,20 @@ public interface MessageMapper {
 
     // 修改状态
     int updateStatus(List<Integer> ids, int status);
+
+    // 查询某个主题下的最新的通知
+    Message selectLatestNotice(int userId, String topic);
+
+    // 查询某个主题下的所有通知数量
+    int selectNoticeCount(int userId, String topic);
+
+    // 查询未读的通知数量
+    /*
+    若topic不为空则表示查询某个会话的未读消息数量
+    若为空则表示查询所有的未读消息数量
+     */
+    int selectNoticeUnreadCount(int userId, String topic);
+
+    // 查询某个主题所包含的通知列表
+    List<Message> selectNotices(int userId, String topic, int offset, int limit);
 }
